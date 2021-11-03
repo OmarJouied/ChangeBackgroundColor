1c414cb40cefe37a4352c2715f6d2edff96d9f4d
OmarJouied <omarjouied1421@yahoo.com>
2021-11-03 18:27:56 +0100

    أنجزت مشروع تغيير الخلفية

---
+++ script.js
@@ -0,0 +1,33 @@
+let HexNum = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f']
+
+var r = 0
+var g = 0
+var b = 0
+
+let red = document.querySelector("#red");
+let green = document.querySelector("#green");
+let blue = document.querySelector("#blue");
+let body = document.querySelector("body");
+
+var event = setInterval(() => {
+    red.onclick = () => {
+        r++;
+        r %= 16;
+        red.textContent = HexNum[r];
+    }
+
+    green.onclick = () => {
+        g++;
+        g %= 16;
+        green.textContent = HexNum[g];
+    }
+
+    blue.onclick = () => {
+        b++;
+        b %= 16;
+        blue.textContent = HexNum[b];
+    }
+    console.log('#' + HexNum[r] + HexNum[g] + HexNum[b])
+    body.style.backgroundColor = '#' + HexNum[r] + HexNum[g] + HexNum[b];
+    
+},500);
