7e656eb9b2ec635fa00af9ee4bf0683aec97a6bd
OmarJouied <87611866+OmarJouied@users.noreply.github.com>
2022-01-30 08:50:51 +0100

    update var with let keyword

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 let HexNum = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f']
 
-var r = 0
-var g = 0
-var b = 0
+let r = 0
+let g = 0
+let b = 0
 
 let red = document.querySelector("#red");
 let green = document.querySelector("#green");
 let blue = document.querySelector("#blue");
 let body = document.querySelector("body");
 
-var event = setInterval(() => {
+let event = setInterval(() => {
     red.onclick = () => {
         r++;
         r %= 16;
